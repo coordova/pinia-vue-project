45371d52a5d794de2d22abbb1c170a2ca16f3144
coordova <grcordova@gmail.com>
2022-11-09 17:17:11 -0400

    Learn Vue3 - Ep 26 - Code Organization

---
+++ src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This is the contact page</h1>
+</template>
+
+<script>
+export default {
+  name: "ContactView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex justify-between">
+    <div>
+      <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400" :disabled="true">Add Member (0 Spots Left)</button>
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
