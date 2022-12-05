9488d2f9283ec7f848877452c3bd719107f5ef76
coordova <grcordova@gmail.com>
2022-12-05 18:45:19 -0400

    Learn Vue3 - Ep 28-29 - Build a modal component - Two ways to transition

---
+++ src/components/Teams/Modal.vue
@@ -0,0 +1,91 @@
+<script setup>
+  defineProps({
+    show: Boolean
+  });
+</script>
+
+<template>
+  <!--<Transition
+    enter-from-class="transition duration-300 opacity-0"
+    enter-to-class="transition duration-200 opacity-100"
+    leave-from-class="transition duration-300 opacity-100"
+    leave-to-class="transition duration-200 opacity-0"
+  >-->
+  <!--<Transition
+    enter-from-class="opacity-0 scale-125"
+    enter-to-class="opacity-100 scale-100"
+    enter-active-class="transition duration-300"
+    leave-active-class="transition duration-200"
+    leave-from-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-125"
+  >-->
+  <Transition name="modal">
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container rounded-lg">
+        <header>
+          <slot name="header">default header</slot>
+        </header>
+
+        <div>
+          <slot name="default">default body</slot>
+        </div>
+
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('close')">Close</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgba(0,0,0,.6);
+  display: grid;
+  place-items: center;
+}
+
+.modal-container {
+  background: white;
+  padding: 1rem;
+  display: grid;
+  position: center;
+  width: 80vw;
+  max-width: 500px;
+  border-radius: 7px;
+}
+
+.modal-footer {
+  border-top: 1px solid #ddd;
+  margin-top: 1rem;
+  padding-top: 0.5rem;
+  font-size: .8rem;
+}
+
+.modal-footer button {
+  background: #ddd;
+  padding: .25rem .75rem;
+  border-radius: 20px;
+}
+
+.modal-footer button:hover {
+  background: #c8c8c8;
+}
+
+/*# Transiciones */
+.modal-enter-active, .modal-leave-active {
+  transition: opacity 1s;
+}
+
+.modal-enter-from, .modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-to, .modal-leave-from {
+  opacity: 100;
+}
+</style>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -7,6 +7,9 @@
 
   // import el dedicated Pinia store (TeamStore) for managing the state for a team
   import {useTeamStore} from "@/stores/TeamStore";
+  import Modal from "@/components/Teams/Modal.vue";
+  import {ref} from "vue";
+
   // lo asignamos a una variable
   let team = useTeamStore();
 
@@ -17,8 +20,9 @@
   // ej.
   setTimeout(() => {
     team.grow(20);
-  }, 2000);
+  }, 1000);
 
+  let showModal = ref(false);
   // console.log(team)
 </script>
 
@@ -26,7 +30,7 @@
   <div class="container mx-auto p-4">
 
     <!--<TeamHeader :team="team" />-->
-    <TeamHeader />
+    <TeamHeader @add="showModal = true" />
 
     <div class="place-self-center flex flex-col gap-y-3 mx-auto" style="width: 725px">
 
@@ -37,6 +41,26 @@
 
     <!--<TeamFooter :team="team" />-->
     <TeamFooter />
+
+    <Modal :show="showModal" @close="showModal=false">
+      <template #header>
+        <h3 class="font-semibold text-xl">Add member</h3>
+      </template>
+
+      <template #default>
+        <p>Need to add a new member to your team?</p>
+        <form class="mt-6">
+          <div class="flex gap-2">
+            <input type="email" placeholder="Email address..." class="flex-1 py-3">
+            <button class="btn btn-secondary">Add</button>
+          </div>
+        </form>
+      </template>
+
+      <!--<template #footer>
+        Override the footer
+      </template>-->
+    </Modal>
 
   </div>
 
